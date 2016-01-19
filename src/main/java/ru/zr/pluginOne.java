f946fcb04b669b66ec9ce8f279af77ce0dceef00
Zloy Rabadaber <zrabadaber@hotmail.com>
2016-01-19 23:58:52 +0300

    начало

---
+++ src/main/java/ru/zr/pluginOne.java
@@ -0,0 +1,10 @@
+package ru.zr;
+
+/**
+ * Created by zraba on 19.01.2016.
+ */
+public class pluginOne implements IPlugin {
+    public void execute() {
+        System.out.print("Work\n");
+    }
+}
